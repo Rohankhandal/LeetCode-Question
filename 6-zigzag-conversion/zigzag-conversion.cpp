a6cb952dfddd32b6a15be99f321e7c8faabc5050
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2023-11-07 12:04:39 +0530

    Time: 8 ms (75.47%) | Memory: 10.7 MB (56.94%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows==1) return s;
+        
+        vector<string> zigzag(numRows);
+        int i =0 , row =0;
+        bool direction =1;  //1 for Top->Bottom
+
+        while(true)
+        {
+            if(direction)
+            {
+                while(row<numRows && i<s.size())
+                {
+                    zigzag[row++].push_back(s[i++]);
+
+                }
+                row=numRows-2;  
+            }
+            else
+            {
+                while(row>=0 && i<s.size())
+                {
+                    zigzag[row--].push_back(s[i++]);
+
+                }
+                row=1;
+            }
+
+            if(i>=s.size())   break;
+
+            direction = ! direction;  //change direction fromm TB->BT && BT->TB(top to bottom && bottom to top)
+        }
+
+        string ans="";
+        for(int i=0;i<zigzag.size();i++){
+            ans+=zigzag[i];
+            //cout<<zigzag[i]<<endl;
+        }
+
+        return ans;
+    }
+};
