0820405818478b48b265a01e535e2605fedc0d91
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2023-11-12 09:17:54 +0530

    Time: 6 ms (21.41%) | Memory: 11.1 MB (89.05%) - LeetSync

--- 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.cpp
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.cpp
@@ -22,3 +22,15 @@ public:
    
     }
 };
+
+// Find the largest and second largest element
+// Check if the largest of second largest or not. If yes then return index of the largest otherwise return -1.
+
+// int dominantIndex(vector<int>& nums) {
+//     int l1 = 0, l2 = (nums[0] < nums[1])? 0 : 1;
+//     for(int i = 1; i < nums.size(); ++i){
+//         if(nums[i] > nums[l1]) { l2 = l1; l1 = i;}
+//         else if(nums[i] > nums[l2]) l2 = i;
+//     }
+//     return (nums[l1] >= 2*nums[l2])?l1: -1;
+// }
