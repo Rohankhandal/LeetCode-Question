c65d4ce605980f83bcb0a9b368f0f989b6dda835
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2023-11-15 19:45:59 +0530

    Time: 4 ms (48.11%) | Memory: 10.4 MB (7.10%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,12 +1,36 @@
 class Solution {
 public:
+void removeOCCRE(string &s,string &part)
+{
+    int found=s.find(part);
+    if(found != string::npos)
+    {
+        //part string has been located
+        //please remove it
+        string left_part=s.substr(0,found);
+        string right_part=s.substr(found+part.size(),s.size());
+        s=left_part + right_part;
+
+        removeOCCRE(s,part);
+    }
+    else
+    {
+        return ; //base case 
+        //all the occureences of part has been removed from s.
+    }
+}
     string removeOccurrences(string s, string part) {
-        int index=s.find(part);
-        while(index != string::npos)
-        {
-            s=s.erase(index,part.length());
-            index=s.find(part);
-        }
-        return s;
+   //Method -2 Using Reccursion
+   removeOCCRE(s,part);
+   return s;
+   
+   //MEthod-1:-    
+        // int index=s.find(part);
+        // while(index != string::npos)
+        // {
+        //     s=s.erase(index,part.length());
+        //     index=s.find(part);
+        // }
+        // return s;
     }
 };
