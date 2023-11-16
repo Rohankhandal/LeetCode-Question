7963e0d9d397dcf066a7ffcb0234258cd0821b3e
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2023-11-16 14:06:06 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (95.00%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -21,3 +21,20 @@ class Solution
             return ans;
         }
 };
+//Method -2 without using other variable
+// class Solution {
+// public:
+//     string defangIPaddr(string address) {
+//         int i=0;
+//         while(i<address.length()){
+//             if(address[i]=='.'){
+//                 address=address.substr(0,i)+"[.]"+address.substr(i+1,-1);
+//                 i=i+2;
+//             }
+//             else{
+//                 i++;
+//             }
+//         }
+//         return address;
+//     }
+// };
