24b5b522fc3a4fe12c67b003551c6b51c4843aa5
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2023-11-16 17:29:13 +0530

    Time: 3 ms (44.22%) | Memory: 6.8 MB (24.44%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -1,15 +1,27 @@
-class Solution {
-public:
-    int numJewelsInStones(string jewels, string stones) {
-        int count=0;
-        for(int i=0;i<jewels.size();i++){
-            for(int j=0; j<stones.size();j++)
+class Solution
+{
+    public:
+        int numJewelsInStones(string jewels, string stones)
+        {
+            int count = 0;
+           	// for(int i=0;i < jewels.size();i++){
+           	//     for(int j=0; j < stones.size();j++)
+           	//     {
+           	//         if(jewels[i] == stones[j])
+           	//             count++;
+           	//     }
+           	// }
+            vector<int>hashTable(256,0);  //create hashTable
+            for(auto it:jewels)
             {
-                if(jewels[i] == stones[j])
+                hashTable[it]=1;     //make true at values of Jewels
+            }
+            for(auto it:stones)
+            {
+                if(hashTable[it])   //if coressponding char found in hashTable then increment count by 1
                     count++;
             }
-        }
 
-        return count;
-    }
+            return count;
+        }
 };
