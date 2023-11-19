37b9d788790756c3cddaeda66832755ddf541f85
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2023-11-19 14:08:42 +0530

    Time: 78 ms (14.23%) | Memory: 11.7 MB (91.76%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -7,24 +7,57 @@ public:
     return (str.find(ch) != string::npos);
     }
     string sortVowels(string s) {
-       string temp;
-       for(char &ch:s)
-       {
-           if(isVowel(ch))
-           {
-               temp.push_back(ch);
-           }
-       } 
-       sort(begin(temp),end(temp));
-       int j=0;
-       for(int i=0;i<s.length();i++)
-       {
-           if(isVowel(s[i]))
-           {
-               s[i]=temp[j];
-               j++;
-           }
-       }
-       return s;
+        //Second Method -2:Using Map
+        unordered_map<char,int> mp;
+
+        for(char &ch:s)
+        {
+            if(isVowel(ch))
+            {
+                mp[ch]++;
+            }
+
+        }
+
+        string temp="AEIOUaeiou";
+        int j=0;  //pointing to first element of temp
+
+        for(int i=0;i<s.length(); i++)
+        {
+            if(isVowel(s[i]))
+            {
+                //vowel=temp[i];
+                while(mp[temp[j]] == 0)
+                {
+                    j++;
+                }
+                s[i]=temp[j];
+                mp[temp[j]]--;
+            }
+        }
+        return s;
+
+
+
+    //First basic method 
+    //    string temp;
+    //    for(char &ch:s)
+    //    {
+    //        if(isVowel(ch))
+    //        {
+    //            temp.push_back(ch);
+    //        }
+    //    } 
+    //    sort(begin(temp),end(temp));
+    //    int j=0;
+    //    for(int i=0;i<s.length();i++)
+    //    {
+    //        if(isVowel(s[i]))
+    //        {
+    //            s[i]=temp[j];
+    //            j++;
+    //        }
+    //    }
+    //    return s;
     }
 };
