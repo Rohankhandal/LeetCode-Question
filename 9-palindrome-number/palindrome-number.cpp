1695b507849b469f3549a0582c4cfa04b8872395
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2023-11-21 11:07:10 +0530

    Time: 7 ms (72.14%) | Memory: 6.4 MB (39.36%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,21 +1,40 @@
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         int temp=x;
+//         int sum=0;
+//         if(x<0)
+//             return false;
+//         while(temp!=0)
+//         {
+//             int rem=temp%10;
+//             if(sum>=INT_MAX/10)
+//                 return false;
+//             sum=sum*10+rem;
+//             temp/=10;
+//         }
+//         if(sum==x)
+//             return true;
+//         else 
+//             return false;
+//     }
+// };
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int temp=x;
-        int sum=0;
-        if(x<0)
+        if (x < 0) {
             return false;
-        while(temp!=0)
-        {
-            int rem=temp%10;
-            if(sum>=INT_MAX/10)
-                return false;
-            sum=sum*10+rem;
-            temp/=10;
         }
-        if(sum==x)
-            return true;
-        else 
-            return false;
+
+        long long reversed = 0;
+        long long temp = x;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            reversed = reversed * 10 + digit;
+            temp /= 10;
+        }
+
+        return (reversed == x);
     }
 };
