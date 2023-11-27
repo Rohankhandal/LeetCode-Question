88638512007f526200bf69109b5c9dd979255b40
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2023-11-28 00:49:35 +0530

    Time: 4 ms (80.69%) | Memory: 7.5 MB (81.76%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,21 +1,39 @@
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         string res;
+//         for (int i = 0; i < s.length(); i++)
+//         {
+//             if (s[i] >= 'A' && s[i] <= 'Z') res += (s[i] + ('a' - 'A'));
+//             else if (s[i] >= 'a' && s[i] <= 'z') res += s[i];
+//             else if (s[i] >= '0' && s[i] <= '9') res += s[i];
+//         }
+//         int i = 0;
+//         int j = res.length() - 1;
+//         while (i < j)
+//         {
+//             if (res[i] != res[j]) return false;
+//             i++;
+//             j--;
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string res;
-        for (int i = 0; i < s.length(); i++)
-        {
-            if (s[i] >= 'A' && s[i] <= 'Z') res += (s[i] + ('a' - 'A'));
-            else if (s[i] >= 'a' && s[i] <= 'z') res += s[i];
-            else if (s[i] >= '0' && s[i] <= '9') res += s[i];
-        }
-        int i = 0;
-        int j = res.length() - 1;
-        while (i < j)
-        {
-            if (res[i] != res[j]) return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
+       int start=0;
+       int end=s.size()-1;
+       while(start<=end){
+           if(!isalnum(s[start])){start++; continue;}
+           if(!isalnum(s[end])){end--;continue;}
+           if(tolower(s[start])!=tolower(s[end]))return false;
+           else{
+               start++;
+               end--;
+           }
+       }
+       return true;
+}
 };
