00bdd5b4588cc45283670e6bdf5c85d0b025db1f
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2023-11-28 18:30:32 +0530

    Time: 11 ms (7.00%) | Memory: 7.1 MB (17.78%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        char ch[26]={0};
-         for(int i=0;i<26;i++)
-        {
-            cout<<ch[i]<<" ";
-        }
+        int arr[26]={0};
+        
         for(int i=0;i<sentence.size();i++)
         {
             int index=(sentence[i]-97);       //sub 97 instead of 96 bcz hashmap start from index 0
             cout<<index<<" ";
-            ch[index]=1;
+            arr[index]=1;
         }
         for(int i=0;i<26;i++)
         {
-            if(ch[i]!=1)
+            if(arr[i]!=1)
             {
                 return false;
             }
