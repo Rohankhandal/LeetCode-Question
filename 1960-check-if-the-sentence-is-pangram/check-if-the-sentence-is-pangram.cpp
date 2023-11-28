ec9aa9c7bccf1116ed7785c28f4077d40dd0f54d
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2023-11-28 18:25:35 +0530

    Time: 16 ms (7.00%) | Memory: 6.9 MB (41.07%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        char ch[26]={0};
+        for(int i=0;i<sentence.size();i++)
+        {
+            int index=(sentence[i]-97);       //sub 97 instead of 96 bcz hashmap start from index 0
+            cout<<index<<" ";
+            ch[index]=1;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(ch[i]!=1)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
