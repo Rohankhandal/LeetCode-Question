048f3729fd7640c9af3c1902e92e0d68d12f7edd
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2023-12-01 16:54:04 +0530

    Time: 110 ms (71.42%) | Memory: 128.4 MB (97.11%) - LeetSync

---
+++ 134-gas-station/gas-station.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        //kitna petrol kam padgya
+        int deficit=0;
+        //kitna petrol bacha hua h
+        int balance=0;
+        //circuit kaha se start kree ho
+        int start=0;
+
+        for( int i=0;i<gas.size();i++)
+        {
+            balance+=gas[i] - cost[i];
+            if(balance < 0)
+            {
+                //yahi pr galti hogi
+                deficit = deficit + abs(balance);
+                start=i+1;
+                balance=0;
+            }
+        }
+        if(balance >= deficit )
+        {
+            return start;
+        }
+       
+            return -1;
+
+    }
+};
