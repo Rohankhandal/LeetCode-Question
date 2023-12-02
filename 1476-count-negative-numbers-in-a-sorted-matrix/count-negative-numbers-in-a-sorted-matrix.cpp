de797ce98dd19b3adb2a4ed89a01a3a6b4543e90
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2023-12-02 14:03:10 +0530

    Time: 16 ms (22.25%) | Memory: 11.4 MB (7.21%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,30 +1,59 @@
+// Method -2
 class Solution {
 public:
+    int bs(vector<int> v, int col){
+        int low=0, high=col-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(v[mid]<0)high=mid-1;
+            else low=mid+1;
+        }
+        if(low>=col)return 0;
+        return col-low;
+    }
     int countNegatives(vector<vector<int>>& grid) {
-       int m=grid.size();
-       int count=0;
-       for(int i=0;i<m;i++)
-       {
-           int n=grid[i].size();
-           int temp=n;
-           int l=0;
-           int e=n-1;
-           while(l<=e)
-           {
-               int mid=(l+e)/2;
-                if(grid[i][mid]<0)
-                {
-                    count+=(temp-mid);
-                    temp=mid;
-                    e=mid-1;
-                }
-                else{
-                    l=mid+1;
-                }
-           }
-
-       } 
-
-       return count;
+        int r=grid.size();
+        int c=grid[0].size();
+        int i,j;
+        int count=0;
+        for(i=0;i<r;i++){
+            count=count+bs(grid[i],c);
+        }
+        return count;
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     int countNegatives(vector<vector<int>>& grid) {
+//        int m=grid.size();
+//        int count=0;
+//        for(int i=0;i<m;i++)
+//        {
+//            int n=grid[i].size();
+//            int temp=n;
+//            int l=0;
+//            int e=n-1;
+//            while(l<=e)
+//            {
+//                int mid=(l+e)/2;
+//                 if(grid[i][mid]<0)
+//                 {
+//                     count+=(temp-mid);
+//                     temp=mid;       //main code to remove a bug which is occur when full row is negative
+//                     e=mid-1;
+//                 }
+//                 else{
+//                     l=mid+1;
+//                 }
+//            }
+
+//        } 
+
+//        return count;
+//     }
+// };
