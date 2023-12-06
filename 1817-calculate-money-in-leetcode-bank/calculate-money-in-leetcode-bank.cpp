609d224575ed838b11fcb2125a959637b2b1b89e
Rohankhandal <84795512+Rohankhandal@users.noreply.github.com>
2023-12-06 23:25:48 +0530

    Time: 2 ms (49.74%) | Memory: 6.4 MB (35.53%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -1,19 +1,36 @@
-class Solution {
-public:
-    int totalMoney(int n) {
-        int i=0,M=0,temp;
-        int count=0;
-        while(i<n)
+class Solution
+{
+    public:
+        int totalMoney(int n)
         {
-            if(i%7==0)
-            {
-                M+=1;
-            }
-            temp=i%7;
-            count+=(M+temp);
+        	//O(1) approach
+            int terms = n / 7;   //weeks
+            int first = 28;
+            int last = 28 + (terms - 1) *7;   //AP formula for nth terms
+            int result = terms *(first + last) / 2;   //sum of nth terms in an AP
 
-            i++;
+           	//final week remaining days=n%7
+            int start_money = 1 + terms;
+            for (int day = 1; day <= (n % 7); day++) {
+                result+=start_money;
+                start_money++;
+            }
+        return result;
         }
-        return count;
-    }
 };
+
+// Simple approach
+// int i=0,M=0,temp;
+//         int count=0;
+//         while(i < n)
+//         {
+//             if(i%7==0)
+//             {
+//                 M+=1;
+//             }
+//             temp=i%7;
+//             count+=(M+temp);
+
+//             i++;
+//         }
+//         return count;
